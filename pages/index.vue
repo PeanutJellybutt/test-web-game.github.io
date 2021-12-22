4c189efa1a2dd73f9bc54c55cbd177148f5809c1
YaniMoo <yanee.moo.1995@gmail.com>
2021-12-22 22:08:49 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   ]"> 
         <pre style="position: absolute;color: white;top: 0;left: 0;background-color: black;z-index: 500;opacity: 0.5">
         isDesktop {{$device.isDesktop}} {{deviceType}}</pre>
-  <div v-if="!isLoading" :class="['panel-bg-main', deviceType]" id="panel-bg-main">
+  <div class="panel-bg-main" id="panel-bg-main">
     <div class="gm4html5_div_class" id="gm4html5_div_id">
         <canvas id="canvas" class="canvas">
           <p>Your browser doesn't support HTML5 canvas.</p>
@@ -25,7 +25,7 @@ export default {
       // cH: 0,
       iH: 0,
       isLoading: true,
-      deviceType: "mobile"
+      deviceType: "sssssss"
     };
   },
   // computed: {
@@ -33,21 +33,24 @@ export default {
   //     return this.$device.isDesktop  ? 'desktop' : 'mobile';
   //   }
   // },
-  watch: {
-    "$device.isDesktop"(newValue, oldValue) {
-      this.deviceType = newValue ? 'desktop' : 'mobile';
-    }
-  },
-  created: async function () {
+  // watch: {
+  //   "$device.isDesktop"(newValue, oldValue) {
+  //     this.deviceType = newValue ? 'desktop' : 'mobile';
+  //   }
+  // },
+  mounted: async function () {
     if (process.client) {
       this.iW = window.innerWidth;
       this.iH = window.innerHeight;
       // this.cW = document.getElementById("panel-bg-main").clientWidth;
       // this.cH = document.getElementById("panel-bg-main").clientHeight;
-      await setTimeout(() => { 
-        this.isLoading = false 
-      }, 1);
+      // await setTimeout(() => { 
+      //   this.isLoading = false 
+      // }, 1000);
       this.deviceType = this.$device.isDesktop ? 'desktop' : 'mobile';
+      console.log(this.$device);
+      var element = await document.getElementById("panel-bg-main");
+      element.classList.add(this.deviceType);
     }
   },
   methods: {},
