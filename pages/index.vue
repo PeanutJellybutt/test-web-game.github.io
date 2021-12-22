ddfff1d4838bfb36f91e6a4e5a44fc710a232ae7
YaniMoo <yanee.moo.1995@gmail.com>
2021-12-22 21:59:47 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     'panel-wrap d-flex align-items-center justify-content-center'
   ]"> 
         <pre style="position: absolute;color: white;top: 0;left: 0;background-color: black;z-index: 500;opacity: 0.5">
-        isDesktop {{$device.isDesktop}}</pre>
-  <div v-if="!isLoading" :class="['panel-bg-main', isDesktop]" id="panel-bg-main">
+        isDesktop {{$device.isDesktop}} {{deviceType}}</pre>
+  <div v-if="!isLoading" :class="['panel-bg-main', deviceType]" id="panel-bg-main">
     <div class="gm4html5_div_class" id="gm4html5_div_id">
         <canvas id="canvas" class="canvas">
           <p>Your browser doesn't support HTML5 canvas.</p>
@@ -24,12 +24,18 @@ export default {
       iW: 0,
       // cH: 0,
       iH: 0,
-      isLoading: true
+      isLoading: true,
+      deviceType: "mobile"
     };
   },
-  computed: {
-    isDesktop() {
-      return this.$device.isDesktop  ? 'desktop' : 'mobile';
+  // computed: {
+  //   deviceType() {
+  //     return this.$device.isDesktop  ? 'desktop' : 'mobile';
+  //   }
+  // },
+  watch: {
+    "$device.isDesktop"(newValue, oldValue) {
+      this.deviceType = newValue ? 'desktop' : 'mobile';
     }
   },
   created: async function () {
@@ -38,7 +44,10 @@ export default {
       this.iH = window.innerHeight;
       // this.cW = document.getElementById("panel-bg-main").clientWidth;
       // this.cH = document.getElementById("panel-bg-main").clientHeight;
-      await setTimeout(() => { this.isLoading = false }, 2000);
+      await setTimeout(() => { 
+        this.isLoading = false 
+      }, 1);
+      this.deviceType = this.$device.isDesktop ? 'desktop' : 'mobile';
     }
   },
   methods: {},
