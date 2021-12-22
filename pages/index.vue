8ed32df61c95ff5fe062cb9417d27edfa4498dc0
YaniMoo <yanee.moo.1995@gmail.com>
2021-12-22 20:58:24 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     'panel-wrap d-flex align-items-center justify-content-center', $device.isDesktop ? 'desktop' : 'mobile'
   ]"> 
         <div style="position: absolute;color: white;top: 0;left: 0;background-color: black;z-index: 500;">
-         isDesktop={{$device}} cW={{cW}} iW={{iW}} cH={{cH}} iH={{iH}}</div>
+         isDesktop={{$device}}</div>
   <div class="panel-bg-main" id="panel-bg-main">
     <div class="gm4html5_div_class" id="gm4html5_div_id">
         <canvas id="canvas" class="canvas">
