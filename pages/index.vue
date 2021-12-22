1f2547297616b939d5720fc80434df7a5b2e56c3
YaniMoo <yanee.moo.1995@gmail.com>
2021-12-22 21:10:39 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 <div id="panel-wrap" :class="[
-    'panel-wrap d-flex align-items-center justify-content-center', !$device.isDesktop ? 'mobile' : 'desktop'
+    'panel-wrap d-flex align-items-center justify-content-center', $device.isDesktop  === true ? 'desktop' : 'mobile'
   ]"> 
         <pre style="position: absolute;color: white;top: 0;left: 0;background-color: black;z-index: 500;opacity: 0.5">
-         isDesktop={{$device}}</pre>
+         {{$device}}</pre>
   <div class="panel-bg-main" id="panel-bg-main">
     <div class="gm4html5_div_class" id="gm4html5_div_id">
         <canvas id="canvas" class="canvas">
@@ -19,9 +19,21 @@
 export default {
   name: "HomePage",
   data() {
-    return {};
+    return {
+      // cW: 0,
+      // iW: 0,
+      // cH: 0,
+      // iH: 0,
+    };
   },
-  created: async function () {},
+  created: async function () {
+    if (process.client) {
+      // this.iW = window.innerWidth;
+      // this.iH = window.innerHeight;
+      // this.cW = document.getElementById("panel-bg-main").clientWidth;
+      // this.cH = document.getElementById("panel-bg-main").clientHeight;
+    }
+  },
   methods: {},
 };
 </script>
