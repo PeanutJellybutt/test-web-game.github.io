dfd272ff064821d760447a07658d9bca08dc4b37
YaniMoo <yanee.moo.1995@gmail.com>
2021-12-22 21:50:17 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   ]"> 
         <pre style="position: absolute;color: white;top: 0;left: 0;background-color: black;z-index: 500;opacity: 0.5">
         isDesktop {{$device.isDesktop}}</pre>
-  <div :class="['panel-bg-main', isDesktop]" id="panel-bg-main">
+  <div v-if="!isLoading" :class="['panel-bg-main', isDesktop]" id="panel-bg-main">
     <div class="gm4html5_div_class" id="gm4html5_div_id">
         <canvas id="canvas" class="canvas">
           <p>Your browser doesn't support HTML5 canvas.</p>
@@ -24,6 +24,7 @@ export default {
       iW: 0,
       // cH: 0,
       iH: 0,
+      isLoading: true
     };
   },
   computed: {
@@ -37,6 +38,7 @@ export default {
       this.iH = window.innerHeight;
       // this.cW = document.getElementById("panel-bg-main").clientWidth;
       // this.cH = document.getElementById("panel-bg-main").clientHeight;
+      await setTimeout(() => { this.isLoading = false }, 2000);
     }
   },
   methods: {},
